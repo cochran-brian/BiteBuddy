c0eaab088bc3094d668bed67c433fdd96b673922
Brian Cochran <cochran.brian@yahoo.com>
2023-10-16 08:19:56 -0500

    Added text edits into AuthScreen (still need to make them KeyboardAvoidingViews)

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ScreenHandler from './app/screenHandler';
+import ScreenHandler from './app/ScreenHandler';
 
-import { useFonts } from 'expo-font';
-
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Open-Sans': require('./app/assets/fonts/OpenSans-ExtraBold.ttf'),
-});
-
-
-  if(fontsLoaded) {
-    return(
-    <ScreenHandler/>
-  );
-}}
-
-=======
+//import { useFonts } from 'expo-font';
 
 export default function App() {
+  // const [fontsLoaded] = useFonts({
+  //   'Open-Sans': require('app/assets/fonts/OpenSans-ExtraBold.ttf'),
+  // });
+
+
+  // if(fontsLoaded) {
+  //   return(
+  //   <ScreenHandler/>
+  //   );
+  // }
   return (
     <ScreenHandler/>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
--- app/screens/AuthScreen.js
+++ app/screens/AuthScreen.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View, Pressable } from 'react-native';
+import { Keyboard, StyleSheet, Text, View, Pressable, TextInput, KeyboardAvoidingView } from 'react-native';
 import { useFonts } from 'expo-font';
 import { useState } from 'react';
 import colors from '../config/colors';
@@ -7,9 +7,10 @@ import colors from '../config/colors';
 export default function AuthScreen() {
 
   const [signingIn,setSigningIn] = useState(true);
+  const [email, setEmail] = useState("");
 
   const [fontsLoaded] = useFonts({
-      'OpenSans-ExtraBold': require('../assets/fonts/OpenSans-ExtraBold.ttf'),
+      'Open Sans': require('../assets/fonts/OpenSans-ExtraBold.ttf'),
   });
 
     if(!fontsLoaded){
@@ -18,9 +19,12 @@ export default function AuthScreen() {
 
     
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-     <View style={styles.container}>
-        <Text style={styles.header}>BITE BUDDY</Text>
+    
+
+    // <KeyboardAvoidingView behavior='padding'>
+    <Pressable styles={styles.container} onPress={() => Keyboard.dismiss()}>
+      
+      <Text style={styles.header}>BITE BUDDY</Text>
         <View style={{flexDirection: 'row'}}>
               <Pressable onPress={() => setSigningIn(true)} style={[styles.viewSelectButton,{backgroundColor: signingIn? colors.primary : colors.neutral, borderTopLeftRadius: 20, borderBottomLeftRadius: 20}]}>
                 <Text style={[styles.viewChangeText, {color: signingIn? 'white' : 'black'}]}>Log In</Text>
@@ -28,11 +32,22 @@ export default function AuthScreen() {
               <Pressable onPress = {() => setSigningIn(false)} style={[styles.viewSelectButton,{backgroundColor: signingIn? colors.neutral : colors.primary, borderTopRightRadius: 20, borderBottomRightRadius: 20}]}>
                 <Text style={[styles.viewChangeText, {color: signingIn? 'black' : 'white'}]}>Sign Up</Text>
               </Pressable>
-      </View>
-     </View>
-
-     
-   </TouchableWithoutFeedback>
+        </View>
+      
+        
+        <View style={[styles.viewTextInput, {marginTop: 123}]}>
+          <TextInput style={styles.textInput} onChangeText={(email) => setEmail(email)} placeholder='Enter email' autoCapitalize='none' keyboardType='email-address' />
+        </View>
+        <View style={[styles.viewTextInput, {marginTop: 11}]}>
+          <TextInput style={styles.textInput} onChangeText={(email) => setEmail(email)} placeholder='Enter phone number' autoCapitalize='none' keyboardType='phone-pad' />
+        </View>
+        <View style={[styles.viewTextInput, {marginTop: 11}]}>
+          <TextInput style={styles.textInput} onChangeText={(email) => setEmail(email)} placeholder='Enter password' autoCapitalize='none' keyboardType='default' />
+        </View>
+        
+        
+      </Pressable>
+      // </KeyboardAvoidingView>
   );
 }
 
@@ -44,7 +59,7 @@ const styles = StyleSheet.create({
   },
   header:{
     color: 'black',
-    fontFamily: 'OpenSans-ExtraBold',
+    fontFamily: 'Open Sans',
     fontSize: 50,
     marginTop: 80,
   },
@@ -58,7 +73,23 @@ const styles = StyleSheet.create({
   },
   viewChangeText:{
     color: 'white',
-    fontFamily: 'OpenSans-ExtraBold',
+    fontFamily: 'Open Sans',
     fontSize: 16
+  },
+  viewTextInput:{
+    width: 270,
+    height: 54,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+    background: 'white',
+  },
+  textInput:{
+    fontFamily: 'Open Sans',
+    width: '100%',
+    height: '100%',
+    fontSize: 14,
+    fontWeight: 300,
+    paddingLeft: 10,
   },
 });
