f38e10fafc48f8315dba5c51230eded3333cd719
Brian Cochran <cochran.brian@yahoo.com>
2023-10-18 11:57:09 -0500

    Update AuthScreen.js

--- app/screens/AuthScreen.js
+++ app/screens/AuthScreen.js
@@ -79,13 +79,13 @@ export default function AuthScreen({navigation}) {
             {changeDisplay()}
             
         </Pressable>
-      </KeyboardAwareScrollView>
 
         <View style={{flex: 1, justifyContent: 'flex-end'}}>
          <TouchableHighlight style= {styles.bottomButton} onPress={onSubmitPressed} underlayColor={colors.primaryDark}>
           <Text style={{color: 'white', fontFamily: 'Open Sans', fontSize: 20}}>SUBMIT</Text>
          </TouchableHighlight>
         </View>
+      </KeyboardAwareScrollView>
     </View>
   
   );
