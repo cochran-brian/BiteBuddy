b90fead44aef19075f0d0865faca4eb178d88bfa
Brian Cochran <cochran.brian@yahoo.com>
2023-11-07 12:30:47 -0600

    Adding more data to database and adjusting structure

--- app/screens/CreateScreen.js
+++ app/screens/CreateScreen.js
@@ -14,17 +14,30 @@ export default function CreateScreen({navigation}) {
   const [locationLat, setLocationLat] = useState('42.095271881586406');
 
   async function fetchData(){
-    var data = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+locationLat+'%2C'+locationLong+'&radius='+(slideValue * 1609.34)+'&type=restaurant&key='+process.env.GOOGLE_MAPS_API_KEY)
+    var data = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+locationLat+'%2C'+locationLong+'&radius='+(slideValue * 1609.34)+'&type=restaurant&opennow=true&key='+process.env.GOOGLE_MAPS_API_KEY)
     data = await data.json();
 
     promises = await data.results.slice(0, iterationLimit).map(async (place) => {
       try{
         const response = await fetch('https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference='+place.photos[0].photo_reference+'&key='+process.env.GOOGLE_MAPS_API_KEY);
+        console.log(place.editorial_summary)
         return {
             name: place.name,
             address: place.vicinity, 
             rating: place.rating,
-            imageURL: response.url,
+            price_level: place.price_level,
+            delivery: place.delivery,
+            reservable: place.reservable,
+            takeout: place.takeout,
+            serves_breakfast: place.serves_breakfast,
+            serves_brunch: place.serves_brunch,
+            serves_lunch: place.serves_lunch,
+            serves_dinner: place.serves_dinner,
+            serves_vegetarian_food: place.serves_vegetarian_food,
+            serves_beer: place.serves_beer,
+            serves_wine: place.serves_wine,
+            website: place.website,
+            image_url: response.url,
             //isOpen: place.opening_hours
           }
       } catch (error) {
@@ -35,10 +48,12 @@ export default function CreateScreen({navigation}) {
 
     data = await Promise.all(promises);
 
-    const reference = collection(db, "restaurants")
+    const reference = collection(db, "places")
+    //add collection to reference above inside of document "restaurants"
+    //create new reference 
     data.map(async (place) => {
       try {
-        await addDoc(reference, place);
+        await addDoc(reference, place); //have this be the new reference
       } catch (error) {
         console.error(error);
       } 
