37d47f6c942e2f3c345354536cf542c58619d78d
bps78 <90796928+bps78@users.noreply.github.com>
2024-01-25 19:04:46 -0600

    Profile screen UI complete

--- AppClient/app/screens/ProfileScreen.js
+++ AppClient/app/screens/ProfileScreen.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
-import { Dimensions, Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { Dimensions, Image, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase/config';
+import { Feather } from '@expo/vector-icons';
+import colors from '../config/colors';
 
 export default function ProfileScreen({ navigation }) {
 
     const logOut = async () => {
         try {
             await signOut(auth);
+            alert('Successfully Logged Out')
             console.log("user signed out")
         } catch (error) {
             console.error(error);
@@ -26,18 +29,31 @@ export default function ProfileScreen({ navigation }) {
             style={styles.image}
             source={{url: 'https://isobarscience-1bfd8.kxcdn.com/wp-content/uploads/2020/09/default-profile-picture1.jpg'}}/>
             <View style={styles.profileTextContainer}>
-
+              <Text style={styles.subheader}>Ben</Text>
+              <Text style={styles.thirdHeader}>Palatine, IL</Text>
             </View>
+
+            <Pressable onPress={() => console.log('Edit Pressed')}>
+              <Feather name="edit" size={32} color="black" />
+            </Pressable>
          </View>
 
          <View style={styles.settingsContainer}>
-            <View style={styles.settingsSubContainer}>
-                <TouchableHighlight onPress={() => logOut()}><Text>Sign out</Text></TouchableHighlight>
-            </View>
+          <Text style={styles.settingsHeader}>Settings</Text>
          </View>
+         <View style={styles.settingsSubContainer}>
+          <TouchableHighlight 
+              style= {styles.bottomButton} 
+              onPress={() => logOut()} 
+              underlayColor={'darkred'}>
+              <Text 
+                style={styles.bottomButtonText}>
+                  LOG OUT</Text>
+            </TouchableHighlight>
+        </View>
         
         <View style={styles.footerContainer}>
-
+          <Text style={styles.footerText}>Joined on 1.25.23</Text>
         </View>
         </View>
     );
@@ -52,41 +68,81 @@ export default function ProfileScreen({ navigation }) {
       paddingHorizontal: 34,
     },
     header: {
-        color: 'black',
-        fontFamily: 'Open Sans',
-        fontSize: 50,
-        marginTop: 5,
-        alignSelf: 'center'
-      },
-      profileContainer:{
-        width: '100%',
-        flex: 1.5,
-        backgroundColor: 'orange',
-        flexDirection: 'row',
-        alignItems: 'center'
-      },
-      image:{
-        width: 110,
-        height: 110,
-        borderRadius: 55,
-      },
-      profileTextContainer:{
-
-      },
-      settingsContainer:{
-        backgroundColor: 'lightblue',
-        width: '100%',
-        flex: 4
-      },
-      settingsSubContainer:{
-        backgroundColor: 'skyblue',
-        width: '100%',
-        marginTop: 90,
-        flex: 1,
-      },
-      footerContainer:{
-        backgroundColor: 'pink',
-        width: '100%',
-        flex: 1,
-      }
+      color: 'black',
+      fontFamily: 'Open Sans',
+      fontSize: 50,
+      marginTop: 5,
+      alignSelf: 'center'
+    },
+    subheader:{
+      fontFamily: 'Open Sans',
+      fontSize: 32,
+    },
+    thirdHeader:{
+      fontFamily: 'Open Sans',
+      fontSize: 20,
+      color: colors.primaryLight
+    },
+    settingsHeader:{
+      fontFamily: 'Open Sans',
+      justifyContent: 'flex-end',
+      fontSize: 28,
+      marginBottom: 16
+    },
+    profileContainer:{
+      width: '100%',
+      flex: 1.5,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingTop: 12
+    },
+    image:{
+      width: 110,
+      height: 110,
+      borderRadius: 55,
+      marginLeft: 8
+    },
+    profileTextContainer:{
+      marginRight: 16,
+      marginLeft: 26
+    },
+    settingsContainer:{
+      width: '100%',
+      flex: 0.6,
+      justifyContent: 'flex-end',
+    },
+    settingsSubContainer:{
+      backgroundColor: '#d9d9d9',
+      width: '100%',
+      flex: 4,
+      borderRadius: 14,
+      alignItems: 'center'
+    },
+    bottomButton:{
+      width: 292,
+      height: 50,
+      borderRadius: 50,
+      marginTop: 46,
+      backgroundColor: '#e3242b',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    bottomButtonText: {
+      color: 'white', 
+      fontFamily: 'Open Sans', 
+      fontSize: 24
+    },
+    footerContainer:{
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    footerText:{
+      fontSize: 20,
+      color: 'grey',
+      fontFamily: 'Open Sans',
+      marginBottom: 18,
+    }
   });
