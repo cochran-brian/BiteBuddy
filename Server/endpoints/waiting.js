4f194cbd26b2b9872a2a482710ae16a995a626c2
Brian Cochran <77315735+cochran-brian@users.noreply.github.com>
2024-02-08 11:57:32 -0600

    Update waiting.js
    Implementing snapshot listeners to keep listen for new ratings being added to the collection to render names on waiting screen

--- Server/endpoints/waiting.js
+++ Server/endpoints/waiting.js
@@ -10,4 +10,24 @@ router.post("/", async (req, res) => {
     }
 })
 
+const getNames = () => {
+    db.collection('bites').doc(uid).collection('ratings')
+}
+
+const listenForNames = () => {
+    db.collection("cities")
+    .onSnapshot((querySnapshot) => {
+        var cities = [];
+        querySnapshot.forEach((doc) => {
+            cities.push(doc.data().name);
+        });
+        console.log("Current cities in CA: ", cities.join(", "));
+    });
+
+
+
+    unsubscribe();
+
+}
+
 module.exports = router;
