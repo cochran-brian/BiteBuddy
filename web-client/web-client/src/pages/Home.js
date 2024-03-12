5cefe526bd15fc73142f02e66b98d143adcbbb38
Brian Cochran <77315735+cochran-brian@users.noreply.github.com>
2024-03-12 09:29:55 -0500

    Working on joining from website

--- web-client/web-client/src/pages/Home.js
+++ web-client/web-client/src/pages/Home.js
@@ -1,4 +1,11 @@
-const Home = () => {
+import { useEffect } from "react";
+
+function Home() {
+
+  useEffect(() => {
+    console.log("join screen")
+  }, [])
+
     return <h1>Home</h1>;
   };
   
--- web-client/web-client/src/pages/Join.js
+++ web-client/web-client/src/pages/Join.js
@@ -1,5 +1,31 @@
-const Join = () => {
-    return <h1>Join</h1>;
-  };
+import { useEffect } from "react";
+
+function Join({ id }) {
+
+  useEffect(() => {
+    fetchData(id)
+  }, [])
+
+  const fetchData = async (id) => {
+    try {
+      console.log(id)
+      const response = await fetch(`http://localhost:3000/restaurants${id}`, { // apparently "localhost" makes the server host the phone instead of the computer
+        method: "GET",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+          //Authorization: `Bearer ${token}`
+        }
+      }); 
+      const result = await response.json();
+      console.log("result", result)
+    } catch (error) {
+      console.error('Error fetching data:', error); // error handling here
+    }
+  }
+
+  return <h1>Join</h1>;
+};
   
   export default Join;
