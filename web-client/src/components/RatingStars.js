793825b37c142c3b7de07a3fdcc395b8cc9544a1
bps78 <90796928+bps78@users.noreply.github.com>
2024-04-03 08:19:54 -0500

    web-client survey work

---
+++ web-client/src/components/RatingStars.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function RatingStars({rating, width, height}) {
+    var imgPath = "";
+
+    if(rating == 0){
+        imgPath = require('../assets/yelpStars/zero.png')
+    }else if(rating <= 0.75){
+        imgPath = require('../assets/yelpStars/half.png')
+    }else if(rating <= 1.75){
+        imgPath = require('../assets/yelpStars/oneAndHalf.png')
+    }else if(rating <= 2.25){
+        imgPath = require('../assets/yelpStars/two.png')
+    }else if(rating <= 2.75){
+        imgPath = require('../assets/yelpStars/twoAndHalf.png')
+    }else if(rating <= 3.25){
+        imgPath = require('../assets/yelpStars/three.png')
+    }else if(rating <= 3.75){
+        imgPath = require('../assets/yelpStars/threeAndHalf.png')
+    }else if(rating <= 4.25){
+        imgPath = require('../assets/yelpStars/four.png')
+    }else if(rating <= 4.75){
+        imgPath = require('../assets/yelpStars/fourAndHalf.png')
+    }else{
+        imgPath = require('../assets/yelpStars/five.png')
+    }
+
+    return(
+        <img
+        //style={[styles.image, {width: width}, {height: height}]}
+        src={imgPath.default}
+        alt="RatingStars"
+        />
+    )
+}
+
+export default RatingStars;
--- web-client/src/pages/Survey.js
+++ web-client/src/pages/Survey.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import NoPage from "./NoPage";
+import RatingStars from "../components/RatingStars";
 
 export default function Survey() {
   
@@ -13,19 +15,40 @@ export default function Survey() {
   const data = location.state.data;
   console.log('Data:', data);
 
+  const onYelpPress = (place) => {
+      window.location.replace(place.url);
+  }
+
   var comps = [];
   data.forEach(place => {
     comps.push(
       <div>
-        <h3>{place.name}</h3>
+        <div style={styles.container}>
+          <img src={place.image_uri} style={styles.image} alt="Image" />
+          <div style={styles.textContainer}>
+            <p style={styles.textTitle}>{place.name}</p>
+            <p style={[styles.textSubheader, { fontFamily: 'Open Sans Medium' }]}>{'$ • ' + place.categories[0].title}</p>
+            <p style={styles.textSubheader}>{place.location.address1 + ' ' + place.location.city + ', ' + place.location.state}</p>
+          </div>
+          <div style={styles.ratingsStyle}>
+            <div>
+              <RatingStars rating={place.rating}/>
+              <p style={styles.baseText}>Based on 235 reviews</p>
+            </div>
+            <button onClick={onYelpPress(place)} style={styles.yelpButton}>
+              <img src={require('../assets/yelp_logo.png')} style={styles.yelpLogo} alt="Yelp Logo" />
+            </button>
+          </div>
+        </div>
       </div>
+
     )
   });
  
 
   return (
     <div>
-      <Carousel width={600} selectedItem={curSlide} swipeable={false} showArrows={false} showIndicators={false} showStatus={false}>
+      <Carousel width={600} selectedItem={curSlide} swipeable={false} showArrows={false} showIndicators={false} showStatus={false} showThumbs={false}>
         {comps}
       </Carousel>
       <span>
@@ -43,4 +66,8 @@ export default function Survey() {
       </span>
     </div>
   )
+
+  
 };
+
+
